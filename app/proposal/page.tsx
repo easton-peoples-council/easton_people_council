c52af2d6ee87b66776fdb298d624d44707536aee
Malachi Kakembo <malachi.kakembo@ecotricity.co.uk>
2026-02-17 15:37:03 +0000

    push to deploy

--- app/proposal/page.tsx
+++ app/proposal/page.tsx
@@ -3,7 +3,7 @@ export default function ProposalPage() {
     <>
       <h1 className="pageTitle">Proposal</h1>
       <p className="intro">
-        This page will contain the proposal for the Easton People Council. Add your content here.
+        This page will contain the proposal for the Easton People's Council. Add your content here.
       </p>
     </>
   );
