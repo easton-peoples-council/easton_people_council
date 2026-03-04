32c58d19661b1e4846648ce7b9aad16206dc79f6
malachi kakembo <malachi.kakembo@ecotricity.co.uk>
2026-03-04 15:09:31 +0000

    new fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,14 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,400&family=DM+Serif+Display:ital@0;1&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head></head>
       <body>
         <header className="siteHeader">
           <div className="container headerInner">
