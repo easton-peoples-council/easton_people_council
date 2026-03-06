45d30adfa8429784143b92e703b3537105748eb8
Malachi Kakembo <malachikakembo@gmail.com>
2026-03-06 16:12:09 +0000

    Content displaying with preview

--- app/rationale/page.tsx
+++ app/rationale/page.tsx
@@ -7,19 +7,15 @@ export default function RationalePage() {
         <h2>What could a People&apos;s Council do for Easton?</h2>
 
         <h3>Protect what&apos;s already good</h3>
-        <ul>
-          <li>Create a neighbourhood plan to protect community centres and public spaces from big developers.</li>
-          <li>Support existing community organisations to keep doing amazing work with grants.</li>
-          <li>Represent the community officially to local police and health services.</li>
-        </ul>
+        <p>Create a neighbourhood plan to protect community centres and public spaces from big developers.</p>
+        <p>Support existing community organisations to keep doing amazing work with grants.</p>
+        <p>Represent the community officially to local police and health services.</p>
 
         <h3>Build the alternative</h3>
-        <ul>
-          <li>Make decisions collectively through direct democracy (e.g. through neighbourhood assemblies).</li>
-          <li>Set up cooperative businesses (e.g. energy coops) owned and controlled by us.</li>
-          <li>Reclaim buildings for community use.</li>
-          <li>Take back public services so they are closer to the people who use them.</li>
-        </ul>
+        <p>Make decisions collectively through direct democracy (e.g. through neighbourhood assemblies).</p>
+        <p>Set up cooperative businesses (e.g. energy coops) owned and controlled by us.</p>
+        <p>Reclaim buildings for community use.</p>
+        <p>Take back public services so they are closer to the people who use them.</p>
 
         <p>+ much more...!</p>
       </section>
